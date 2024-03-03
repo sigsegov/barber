8ef941b166a1b14bab38e19bb6cdfa43c8cc924d
Marcio Barroso <sigsegov@gmail.com>
2024-03-03 20:15:41 -0300

    feat: add initial search componet

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,25 @@
-import Image from "next/image";
+import { format } from "date-fns"
 import Header from "../_components/header";
+import { ptBR } from "date-fns/locale";
+import Search from "./_component/search";
 
 export default function Home() {
   return (
     <div>
       <Header />
+
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Marcio!</h2>
+        <p className="text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM 'de' yyyy", {
+            locale:ptBR,
+          })}
+        </p>
+      </div>
+      <div className="div px-5 mt-6">
+        <Search />
+      </div>
+
     </div>
   )
 }
