eb3242ec21de66bbff4d9a9778e32a55cda4c7df
Marcio Barroso <sigsegov@gmail.com>
2024-03-04 00:26:37 -0300

    feat: add footer component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -42,6 +42,18 @@ export default async function Home() {
           ))}
         </div>
       </div>
+
+      <div className="mt-6 mb-[4.5rem]">
+        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">Populares</h2>
+
+        <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop) => (
+            <div key={barbershop.id} className="min-w-[167px] max-w-[167px]">
+              <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+            </div>
+          ))}
+        </div>
+      </div>
       </div>
   )
 }
